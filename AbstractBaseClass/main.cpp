5f56e80b6f4578ef08b141253c0bfddbf03f457d
NastyaSkv <NastyaSkv@DESKTOP-S0J6MVO>
2022-11-19 19:44:32 +0300

    abstract

---
+++ AbstractBaseClass/main.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+class Animal
+{
+	std::string name;
+public:
+	virtual void sound()const = 0;
+};
+class Cat :public Animal
+{
+
+};
+class Tiger :public Cat
+{
+public:
+	void sound()const override
+	{
+		cout << "Ppppppppppp" << endl;
+	}
+};
+class Lion :public Cat
+{
+public:
+	void sound()const override
+	{
+		cout << "ўа € теб€ схаваю" << endl;
+	}
+};
+class Dog :public Animal{};
+class Wolf :public Dog
+{
+public:
+	void sound()const override
+	{
+		cout << "Ayyyyyy" << endl;
+	}
+};
+void main()
+{
+	setlocale(LC_ALL, "");
+	//Animal slonohobotan;
+	//Cat tom;
+	/*Tiger tiger;
+	tiger.sound();
+
+	Lion killer;
+	killer.sound();
+
+	Wolf wolf;
+	wolf.sound();*/
+
+	Animal* zoo[] =
+	{
+		new Lion,
+		new Tiger,
+		new Wolf
+	};
+	for (int i = 0; i < sizeof(zoo) / sizeof(Animal*); i++)
+	{
+		zoo[i]->sound();
+	}
+}
